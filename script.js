4a2974fd6bd3b9b9c13ef04a6a10d1a78ad3d4af
Angelo Thys <thys.angelo@gmail.com>
2023-08-16 14:05:30 +0100

    Add a button that resizes the grid

--- script.js
+++ script.js
@@ -4,3 +4,33 @@ for (let i = 0; i < 256; i++) {
     const div = document.createElement("div");
     grid.appendChild(div);
 }
+
+function resizeDivs(squares) {
+    const stylesheet = document.styleSheets[0];
+    console.log(stylesheet);
+    const gridDivs = [...stylesheet.cssRules].find(r => r.selectorText === ".grid > div")
+
+    gridDivs.style.setProperty("height", `${50/squares}vh`);
+    gridDivs.style.setProperty("width", `${50/squares}vw`);
+}
+
+function createGrid() {
+    let squares = +prompt("Indicate the amount of squares per side for the new grid.")
+
+    if (squares > 100) {
+        squares = 100;
+    }
+
+    grid.replaceChildren();
+
+    resizeDivs(squares);
+
+    for (let i = 0; i < squares**2; i++) {
+        const div = document.createElement("div");
+        grid.appendChild(div);
+    }
+}
+
+const button = document.querySelector("button");
+
+button.addEventListener('click', createGrid);
